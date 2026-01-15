18b69a04a10dc291da83592bd8a545777dadcb8f
Chukwuemeka David <dev.ataraxia7@gmail.com>
2026-01-15 21:51:26 +0100

    added octokit client in backend/core, initialized it in oauthService.js, also added getters and initializer

--- backend/core/octokit.client.js
+++ backend/core/octokit.client.js
@@ -1,10 +1,22 @@
 import {Octokit} from "octokit";
 
-// function to create octokit client
-export function createOctoKitClient(token){
-const octokit = new Octokit({
-  auth: token,
+let octokitClient = null;
+
+
+// function to initialize octokit, to br used in the oauth flow
+export function initOctokit (accessToken,callback){
+ octokitClient = new Octokit({
+  auth: accessToken,
 });
 
-return octokit
-;}
+if (callback) callback();
+}
+
+
+// getter function for octokit
+export function getOctokit(){
+  if (!octokitClient){
+    throw new Error("octokit is not initialized")
+  }
+}
+
