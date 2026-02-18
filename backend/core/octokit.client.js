c0699ee85cd3c12a4893704e7975e4f7339a0a01
Chukwuemeka David <dev.ataraxia7@gmail.com>
2026-02-18 04:15:35 +0100

    restructuring

--- backend/core/octokit.client.js
+++ backend/core/octokit.client.js
@@ -2,25 +2,25 @@ import { Octokit } from "octokit";
 
 // let octokitClient = null;
 
-// // function to initialize octokit, to br used in the oauth flow
-// export function initOctokit(accessToken, callback) {
-//   octokitClient = new Octokit({
-//     auth: accessToken,
-//   });
+// function to initialize octokit, to br used in the oauth flow
+export function createOctokit(accessToken, callback) {
+  octokitClient = new Octokit({
+    auth: accessToken,
+  });
 
-//   // alerting that octoKit has been initialized
-//   if (callback) callback();
-// }
-
-// // getter function for octokit
-// export function getOctokit() {
-//   if (!octokitClient) {
-//     throw new Error("octokit is not initialized");
-//   }
-//   return octokitClient;
-// }
-
-// function to create an octokit client.
-export function createOctokit(accessToken) {
-  return new Octokit({ auth: accessToken });
+  // alerting that octoKit has been initialized
+  if (callback) callback();
 }
+
+// // // getter function for octokit
+// // export function getOctokit() {
+// //   if (!octokitClient) {
+// //     throw new Error("octokit is not initialized");
+// //   }
+// //   return octokitClient;
+// // }
+
+// // function to create an octokit client.
+// export function createOctokit(accessToken) {
+//   return new Octokit({ auth: accessToken });
+// }
