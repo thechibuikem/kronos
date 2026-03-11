98c5ec0190aad80d5b74d5657d3a70a696a338fd
Chukwuemeka David <dev.ataraxia7@gmail.com>
2026-03-11 06:01:26 +0100

    fixing server.js for render deploy

--- backend/core/server.js
+++ backend/core/server.js
@@ -31,7 +31,7 @@ const corsOptions = {
 
 //-- mountings
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.json()); // Parse JSON data
@@ -62,7 +62,7 @@ app.get("/api", (req, res) => {
 connectDB()
   .then(() => {
     app.listen(PORT, () =>
-      console.log(`Server running on port:${backendUrl} ${PORT} ✅`)
+      console.log(`Server running on port:${backendUrl}`)
     );
   })
   .catch((err) => console.error("db connection failed", err));
