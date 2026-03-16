fdd9200f5ed3e10f72fc4f0e263140dabe4c922e
Chukwuemeka David <dev.ataraxia7@gmail.com>
2026-03-16 07:37:18 +0100

    cleaning backend-core for testing

--- backend/core/db.js
+++ backend/core/db.js
@@ -1,22 +1,13 @@
-// import dotenv from 'dotenv';
-import mongoose from "mongoose";// import mongoose ODM
-import { redisClient } from './redisClient.js';
-
-// dotenv.config();
-
-// initializing redis
-redisClient.on("error", (err) => console.error("Redis Client Error", err));
-
-await redisClient.connect();
-console.log("Connected to Redis ✅");
+import mongoose from "mongoose";
 
 // async function to connect to mongodb
-const connectDB = async () => {
+async function connectDB   (){
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Connected to MongoDB ✅`); // tell me that I've connected successfully if I have
-  } catch (err) {
-    console.log("error connecting to mongo db", err);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Connected to MongoDB 🌟`);
+  } 
+  catch (err) {
+    console.log("error connecting to mongo db 💣\n", err);
   }
 };
 
--- backend/core/redis.client.js
+++ backend/core/redis.client.js
@@ -14,19 +14,21 @@ export const redisClient = createClient({
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
 
 // 3. create async function that connects to redis
-async function redisConnect() {
+export async function connectRedis() {
   try {
     await redisClient.connect();
+    console.log(`Connected to redis 🌟`);
   } catch (err) {
-    console.log("error occured while connecting to redis\n", err);
+    console.log("error connecting to redis 💣\n", err);
   }
 }
 
-// 4. run redis-connecting function
-await redisConnect();
 
-// so basically here I am checking if mmy refreshToken entry is still in redus cache because it has a TTL and if it's no longer there it means the one in http cookie is invalid
+// 4. retrieving a userId value from redis
 export async function getRefreshTokenFromRedis(refreshToken) {
-  const userId = await redisClient.get(refreshToken); //get the userId from reddis because refreshTokemn is a key
+  const userId = await redisClient.get(refreshToken); //get the userId from reddis because refreshToken is a key
   return userId;
 }
+
+
+// so basically here I am checking if mmy refreshToken entry is still in redis cache because it has a TTL and if it's no longer there it means the one in http cookie is invalid
