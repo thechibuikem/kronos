4cbdea9114b7fe9700bbdc0a999cdfa6f85fdb85
Chukwuemeka David <dev.ataraxia7@gmail.com>
2026-03-17 01:21:03 +0100

    refactored oauthService to fix uri-mismatch issue

--- backend/modules/auth/services/oauthService.js
+++ backend/modules/auth/services/oauthService.js
@@ -1,13 +1,18 @@
 import { addOauthUser } from "../utils/addOauthUser.js";
-// import { initOctokit } from "../../../core/octokit.client.js";
+import { getUrls } from "../../../core/config.js"
 
-//url to get github code
+// 0. destructuring backend url from getUrls getter function.
+const {backendUrl}= getUrls()
+
+
+//1. Initial service to knock at github's door.
 export function githubOauthService() {
-  return `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${process.env.PROJECT_BASE_URL}/api/auth/github/callback&scope=user:email,repo`; 
+  return `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${backendUrl}/api/auth/github/callback&scope=user:email,repo`; 
 }
 
-//callback function to get github token using code 
+//2. callback function to get github token using code
 export async function githubTokenService(code) {
+  // .1 post request submitting code to get token response
   const tokenRes = await fetch(`https://github.com/login/oauth/access_token`, {
     method: "POST",
     headers: { Accept: "application/json", "Content-Type": "application/json" },
@@ -16,25 +21,20 @@ export async function githubTokenService(code) {
       client_secret: process.env.GITHUB_CLIENT_SECRET,
       code,
     }),
-  }); //using code to get github access-token
+  });
+  // .2 validating token response
+  if (!tokenRes.ok) throw new Error("GitHub token request failed");
 
-  if (!tokenRes.ok) throw new Error("GitHub token request failed");//if something is wrong with our request to get an access token
-  
-  // retrieving access_token from github
+  //.3 retrieving access token from token response
   const { access_token } = await tokenRes.json();
-  
-  // //=======initialize octoKit client=========
-  // initOctokit(git
-  //   access_token,
-  //   (console.log("octokit client has been initialized successfully")
-  // ))
-  
-  // Fetch user profile
+
+  //.4 Fetching user profile
   const user = await (
     await fetch("https://api.github.com/user", {
-      headers: { Authorization: `Bearer ${access_token}` }
+      headers: { Authorization: `Bearer ${access_token}` },
     })
-  ).json(); //get user details from github
+  ).json();
+  //.5 Fetching user emails
 
   const emails = await (
     await fetch("https://api.github.com/user/emails", {
@@ -42,21 +42,21 @@ export async function githubTokenService(code) {
     })
   ).json();
 
-  
-  
-  // getting our users email to store in on gitHub
   const primary = emails.find((e) => e.primary && e.verified && e.email);
+//  .6 validating emails
   if (!primary) throw new Error("No verified email");
-  
+// .7 retrieving emails
   const email = primary.email;
-  
-  const {status,data,exitingOauthUser} = await addOauthUser(
+
+  // .8 signing up or signing in 
+  const { status, data, exitingOauthUser } = await addOauthUser(
     email,
     user,
-    access_token
+    access_token,
   );
 
-  const redirectUrl = `http://localhost:5173/dashboard` 
+  // front-end redirect uri
+  const redirectUrl = `http://localhost:5173/dashboard`;
 
-  return { redirectUrl,data,status};
+  return { redirectUrl, data, status };
 }
