3e7416ec1a3070e49d8b9601db7783f5fd5f5eb8
Chukwuemeka David <dev.ataraxia7@gmail.com>
2026-03-19 17:08:05 +0100

    refactoring oauth for dev-test

--- backend/modules/auth/utils/addOauthUser.js
+++ backend/modules/auth/utils/addOauthUser.js
@@ -3,14 +3,16 @@ import { userModel } from "../../user/models/userModel.js";
 import { redisClient } from "../../../core/redis.client.js";
 import jwt from "jsonwebtoken";
 
+
 export const addOauthUser = async (email, user,access_token) => {
   try {
     const exitingOauthUser = await userModel.findOne({
       $or: [{ userEmail: email }, { github_id: user.id }],
     }); //find oauth user
 
-    // create a new-user
+    //1. user DNE
     if (!exitingOauthUser) {
+      //.1 create user instance
       const newUser = userModel({
         userEmail: email,
         githubId: user.id,
@@ -20,7 +22,7 @@ export const addOauthUser = async (email, user,access_token) => {
         githubToken:access_token
       });
 
-      // create tokens
+      //.2 create tokens
       const accessToken = jwt.sign(
         { userId: newUser["_id"], roles: newUser["roles"] },
         process.env.ACCESS_TOKEN_SECRET,
@@ -34,15 +36,21 @@ export const addOauthUser = async (email, user,access_token) => {
         }
       );
 
-      //assigning refreshToken to new user
+      //.3 assigning refreshToken to new user
       newUser.refreshToken = refreshToken;
 
-      // creating refresh and
+      //.4 storing refresh token in redis 'w' TTL
+      try{
       redisClient.set(refreshToken, `refresh:${newUser["_id"]}`, {
         EX: 60 * 60 * 24 * 30,
-      }); // Store refresh token in redis.
+      }); 
+      }catch(error){
+        console.log("\nerror saving refreshToken to redis @addNewUser process",error)
+      }
+      
 
-      await newUser.save(); // save newUser
+      //6, saving new user to DB
+      await newUser.save();
       console.log("saved new user successfully");
       return {
         status: 200,
@@ -54,33 +62,39 @@ export const addOauthUser = async (email, user,access_token) => {
       };
     }
 
-    //if user was already existing sign make changes to the mmongo document
+    //2. user Exists
     if (exitingOauthUser) {
-      // creating refresh and access token for authorization
+      //.1 create tokens
       const accessToken = jwt.sign(
         { userId: exitingOauthUser["_id"], roles: exitingOauthUser["roles"] },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "15m" }
+        { expiresIn: "15m" },
       );
-
       const refreshToken = jwt.sign(
         { userId: exitingOauthUser["_id"] },
         process.env.REFRESH_TOKEN_SECRET,
         {
           expiresIn: "30d",
-        }
+        },
       );
-
+      // .2 updating our saved user
       exitingOauthUser.githubId = user.id;
-      exitingOauthUser.repos_url = user.repos_url,
-      exitingOauthUser.refreshToken = refreshToken;
-      exitingOauthUser.githubToken= access_token
+      ((exitingOauthUser.repos_url = user.repos_url),
+        (exitingOauthUser.refreshToken = refreshToken));
+      exitingOauthUser.githubToken = access_token;
 
-      // Store refresh token in redis
-      redisClient.set(refreshToken, `refresh:${exitingOauthUser["_id"]}`, {
-        EX: 60 * 60 * 24 * 30,
-      });
-
+      // .3 Store refresh token in redis
+      try {
+        redisClient.set(refreshToken, `refresh:${exitingOauthUser["_id"]}`, {
+          EX: 60 * 60 * 24 * 30,
+        });
+      } catch (error) {
+        console.log(
+          "\nerror saving refreshToken to redis @addNewUser process",
+          error,
+        );
+      }
+      //6, saving exosting user to DB
       await exitingOauthUser.save();
 
       return {
