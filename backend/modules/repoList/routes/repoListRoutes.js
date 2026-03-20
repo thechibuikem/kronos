4af25356f11f8218ac5790758d170005d07c84e3
Chukwuemeka David <dev.ataraxia7@gmail.com>
2026-03-20 16:42:24 +0100

    Clear main branch - temporary empty state

--- backend/modules/repoList/routes/repoListRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { getAllReposController } from "../controller/repoListController.js"
-
-const router = express.Router()//creating router instance
-
-router.get("/allRepos",getAllReposController)
-
-export default router
--- docs/comingSoon.md
+++
@@ -1,1 +0,0 @@
-<h1 align = "center">Docs coming Soon</h1>
