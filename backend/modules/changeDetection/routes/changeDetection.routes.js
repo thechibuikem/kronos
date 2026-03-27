9136921647502d01560a8bb0653394b3d9d9c190
Chukwuemeka David <dev.ataraxia7@gmail.com>
2026-03-27 13:15:07 +0100

    debuging rm webhook @ server-side

--- backend/modules/changeDetection/routes/changeDetection.routes.js
+++ backend/modules/changeDetection/routes/changeDetection.routes.js
@@ -8,6 +8,6 @@ const router = express.Router()//creating router instance
 //=========routes for change detection service=====//
 router.get("/webhook", webhookDataController);
 router.post("/webhook",addWebhookController)
-router.delete("/webhook",removeWebhookController);
+router.delete("/webhook/:repoId",removeWebhookController);
 
 export default router
