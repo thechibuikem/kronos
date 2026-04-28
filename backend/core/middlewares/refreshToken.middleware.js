16b69a19c9703a04d6bd7029dc19bc45f676cba5
Chukwuemeka David <david.chukwuemeka.swe@gmail.com>
2026-04-28 19:00:29 +0100

    fixing bugs

--- backend/core/middlewares/refreshToken.middleware.js
+++ backend/core/middlewares/refreshToken.middleware.js
@@ -6,7 +6,7 @@ export function verifyrefreshToken(req, res, next) {
     const refreshToken = req.cookies.refreshToken
 
   if (!refreshToken || typeof refreshToken !== "string")
-    console.error("RefreshToken unavailable, at refreshToken middleware")
+    {console.error("RefreshToken unavailable, at refreshToken middleware")
     return res
       .status(401)
       .json({
@@ -15,7 +15,7 @@ export function verifyrefreshToken(req, res, next) {
           code: "INVALID_TOKEN",
         },
       });
-
+    }
    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
      if (err)
     console.error("RefreshToken invalid, at refreshToken middleware");
