f7b2b8ee3fd617fea6d88ae4d55340e6780d5eea
Chukwuemeka David <david.chukwuemeka.swe@gmail.com>
2026-05-04 16:09:14 +0100

    refactoring to fix bullMQ initialization

--- backend/core/queue/analysis.queue.js
+++ backend/core/queue/analysis.queue.js
@@ -1,8 +1,5 @@
 import {Queue} from 'bullmq'
 
 export const analysisQueue = new Queue("analysis-queue", {
-  redis: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-  },
+  connection: process.env.REDIS_URL,
 });
