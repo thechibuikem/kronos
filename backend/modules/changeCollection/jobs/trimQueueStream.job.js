2acc06de92a2e460f468244ad021b17cf65ad3d4
Chukwuemeka David <david.chukwuemeka.swe@gmail.com>
2026-05-06 13:58:54 +0100

    disabling queue event streams

--- backend/modules/changeCollection/jobs/trimQueueStream.job.js
+++ backend/modules/changeCollection/jobs/trimQueueStream.job.js
@@ -1,15 +1,15 @@
-// import cron from "node-cron";
-// import { analysisQueue } from "../../../core/queue/analysis.queue.js";
+import cron from "node-cron";
+import { analysisQueue } from "../../../core/queue/analysis.queue.js";
 
-//  async function trimQueueStream() {
-//     try{
-//         await analysisQueue.client.xtrim("bull:analysis-queue:events", "MAXLEN", "~", 10);
-//     }catch(error){
-//         console.error("queue stream clearing failed: ",error.message)
-//     }
-// }
+ async function trimQueueStream() {
+    try{
+        await analysisQueue.client.xtrim("bull:analysis-queue:events", "MAXLEN", "~", 5);
+    }catch(error){
+        console.error("queue stream clearing failed: ",error.message)
+    }
+}
 
-// export function startTrimQueueStreamCron(){
-//     cron.schedule("*/3 * * * * ", trimQueueStream)
-//     console.log("we just trimmed the queue stream")
-// }
+export function startTrimQueueStreamCron(){
+    cron.schedule("*/3 * * * * ", trimQueueStream)
+    console.log("we just trimmed the queue stream")
+}
