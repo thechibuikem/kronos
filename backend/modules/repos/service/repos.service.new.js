1c5acdee8a656768119344da3c5925b1d3ecb9d4
Chukwuemeka David <david.chukwuemeka.swe@gmail.com>
2026-05-10 21:36:29 +0100

    fixing bug

--- backend/modules/repos/service/repos.service.new.js
+++ backend/modules/repos/service/repos.service.new.js
@@ -15,8 +15,8 @@ export async function getReposFromGithub(user) {
 
   //checking response
 
+  console.log("response from github:\n",response)
   if (response.status === 200) {
-    console.log("response from github",response)
     return response;
   }
 
