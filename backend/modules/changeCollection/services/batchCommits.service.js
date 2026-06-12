822b7bb4bf8196e28ac49d36cf6d8300702b03d7
Chukwuemeka David <david.chukwuemeka.swe@gmail.com>
2026-06-12 03:51:46 +0100

    [fix]: removing un-used code

--- backend/modules/changeCollection/services/batchCommits.service.js
+++ backend/modules/changeCollection/services/batchCommits.service.js
@@ -1,39 +1,5 @@
 import { redisClient } from "../../../core/redis.client.js";
 
-// export async function storeCommitBatch(commits) {
-//   const key = `kron:${commits[0].userId}:commits`;
-
-//   // Store each commit as JSON string
-//   for (const commit of commits) {
-//     await redisClient.lPush(key, JSON.stringify(commit));
-//   }
-// }
-
-// export async function storeCommitBatch(commits) {
-//   const userId = commits[0].userId;
-//   const KronIdSet = new Set();
-//   commits.forEach((commit) => {
-//     KronIdSet.add(commit.kronId);
-//   });
-//   const kronIdArray = [...KronIdSet];
-//   // Store each commit as JSON string
-//   for (const commit of commits) {
-//     await redisClient.lPush(key, JSON.stringify(commit));
-//   }
-
-//   const key = `kron:${commits[0].userId}:${kronId}:commits`;
-// }
-
-
-// export async function storeCommitBatch(commits) {
-//   const key = `kron:${commits[0].userId}:commits`;
-
-//   // Store each commit as JSON string
-//   for (const commit of commits) {
-//     await redisClient.lPush(key, JSON.stringify(commit));
-//   }
-// }
-
 export async function storeCommitBatch(commits) {
   const key = `kron:${commits[0].userId}:${commits[0].kronId}:commits`;
 
