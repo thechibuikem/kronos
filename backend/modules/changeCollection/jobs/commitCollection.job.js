d2cf24f19b5258539433ea0d0fcedb5f69c55353
Chukwuemeka David <david.chukwuemeka.swe@gmail.com>
2026-06-12 03:36:58 +0100

    adjusting kron-time for testing

--- backend/modules/changeCollection/jobs/commitCollection.job.js
+++ backend/modules/changeCollection/jobs/commitCollection.job.js
@@ -59,5 +59,5 @@ export async function collectChanges() {
 }
 
 export function startCollectChangesCron() {
-  cron.schedule("0 0 */6 * * *", collectChanges);
+  cron.schedule("0 */3 * * * *", collectChanges);
 }
