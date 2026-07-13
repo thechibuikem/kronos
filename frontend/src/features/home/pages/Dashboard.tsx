ec21964c9d3f43decd95119f7559e4bd7624cbc4
Chukwuemeka David <david.chukwuemeka.swe@gmail.com>
2026-07-13 20:30:59 +0100

    fix: resolving ui bugs

--- frontend/src/features/home/pages/Dashboard.tsx
+++ frontend/src/features/home/pages/Dashboard.tsx
@@ -1,7 +1,15 @@
 import VerticalNavigation from "@/features/home/components/VerticalNavigation";
 import Layout from "@/features/home/components/Layout";
+import { useFetchKrons } from "@/hooks/useFetchKrons";
+import { useFetchRepos } from "@/hooks/useFetchRepos";
+import { useEffect } from "react";
 
 function Dashboard() {
+useEffect(() => {
+  useFetchRepos()
+  useFetchKrons()
+
+}, []);
   return (
     <section className="flex bg-[#0a0a0f] min-h-screen h-full w-full overflow-x-hidden">
       <VerticalNavigation />
