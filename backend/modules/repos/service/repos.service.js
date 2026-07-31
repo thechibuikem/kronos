ec3bd83777025c80cf75c82b9a8d3594b0446ae7
Chukwuemeka David <david.chukwuemeka.swe@gmail.com>
2026-07-31 10:44:37 +0100

    chore: removed paging of repolist response
    Signed-off-by: Chukwuemeka David <david.chukwuemeka.swe@gmail.com>

--- backend/modules/repos/service/repos.service.js
+++ backend/modules/repos/service/repos.service.js
@@ -8,66 +8,68 @@ export async function getReposFromGithub(user) {
   const octokitClient = createOctokit(user.githubToken);
 
   // Get all repos belonging to this user
-  const response = await octokitClient.request(`GET /users/${user.username}/repos`, {
-    headers: {
-      "X-GitHub-Api-Version": "2026-03-10",
-    },
-  });
+  try {
+    const allRepos = await octokitClient.paginate("GET /user/repos", {
+      headers: { "X-GitHub-Api-Version": "2026-03-10" },
+      type: "owner",
+      affiliation: "owner,collaborator",                      
+    });
 
-  //checking response
-  if (response.status === 200) {
-    return response;
-  }
-
+    return allRepos;
+  } catch (error) {
     console.error({
       message: "repositories from github, is not okay",
       location: "repos/repos.service.js",
-      error: response,
+      error: error,
     });
     return {
       error: {
         message: "repositories from github, is not okay",
       },
-    }
+    };
+  }
 }
+
+
+
 
 // function to get all Repos
 export async function getRepos(user) {
-  const TTL = 60*60*24; // 24 hours
+  const TTL = 60 * 60 * 24; // 24 hours
   const redisRepoKey = `user:${user.id}:repos`;
-  const cached = await redisClient.get(redisRepoKey);
-  
-  
+  // const cached = await redisClient.get(redisRepoKey);
+  const cached = false;
+
   if (cached) {
     console.log("repolist from cache: \n", cached);
     return JSON.parse(cached);
   }
-// cache miss
+
+  // cache miss
   else {
-  console.log("There was a cache miss")
-  const response = await getReposFromGithub(user);
-  // on etag change
-  if (response.status === 200) {
-    const repoList = craftRepoList(response,user)
+    console.log("There was a cache miss");
+    const response = await getReposFromGithub(user);
+    // on etag change
+    // if (response.status === 200) {
+    const repoList = craftRepoList(response, user);
     console.log("repolist from github: \n", console.table(repoList));
-    
 
     // ========= UPDATING MONGODB ==========//
-    await RepoModel.deleteMany({ githubOwnerId: user.id });
-    await Promise.all(
-      repoList.map((repo) => {
-        const newRepo = new RepoModel(repo);
+    // await RepoModel.deleteMany({ githubOwnerId: user.id });
+    // await Promise.all(
+    //   repoList.map((repo) => {
+    //     const newRepo = new RepoModel(repo);
 
-        return newRepo.save();
-      }),
-    );
+    //     return newRepo.save();
+    //   }),
+    // );
 
     // ========= UPDATING REDIS ==========//
-    await redisClient.set(redisRepoKey, JSON.stringify(repoList), { EX: TTL });
+    // await redisClient.set(redisRepoKey, JSON.stringify(repoList), {
+    //   EX: TTL,
+    // });
 
     return repoList; // returning repos to client
-  }
-
+    // }
   }
 }
-
