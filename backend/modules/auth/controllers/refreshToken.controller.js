538ecad1304c77da1f8aa6d57586d4e1ec0a7b23
Chukwuemeka David <david.chukwuemeka.swe@gmail.com>
2026-08-05 00:03:04 +0100

    style: improved code formatting

--- backend/modules/auth/controllers/refreshToken.controller.js
+++ backend/modules/auth/controllers/refreshToken.controller.js
@@ -1,32 +1,32 @@
 import { refreshTokenService } from "../services/refreshToken.service.js";
 
-export async function generateAccessToken(req,res) {
-  try{
+export async function generateAccessToken(req, res) {
+  try {
     // 1. extract refersh token from request cookies
-  const refreshToken = req.cookies.refreshToken;
-  // refresh token service
-const result = await refreshTokenService(refreshToken);
-  if (result.data) {
-     res.status(200).json({data:{
-      accessToken:result.data.accessToken}
-    });
-
-  } else if (result.error) {
-    return res.status(401).json({
-      error:{
-      message:result.error.message,
-      code:"TOKEN_GENERATION_FAILED"
+    const refreshToken = req.cookies.refreshToken;
+    // refresh token service
+    const result = await refreshTokenService(refreshToken);
+    if (result.data) {
+      res.status(200).json({
+        data: {
+          accessToken: result.data.accessToken,
+        },
+      });
+    } else if (result.error) {
+      return res.status(401).json({
+        error: {
+          message: result.error.message,
+          code: "TOKEN_GENERATION_FAILED",
+        },
+      });
     }
+  } catch (error) {
+    console.error("Oauth Error:", error);
+    return res.status(500).json({
+      error: {
+        message: "refresh token failed",
+        code: "REFRESH_TOKEN_FAILED",
+      },
     });
   }
-
-    }
-    catch(error){
-      console.error("Oauth Error:",error);
-        return res.status(500).json({
-          error: {
-            message: "refresh token failed",
-            code: "REFRESH_TOKEN_FAILED",
-          }
-    });
-}}
+}
